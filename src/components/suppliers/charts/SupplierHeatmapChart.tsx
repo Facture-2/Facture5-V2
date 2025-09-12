81c6bb1db4df3abcbb6a277de22b3651bd739be3
yaya25yoyo-beep <yaya25yoyo@gmail.com>
2025-09-12 17:21:01 +0100

    Updated SupplierHeatmapChart.tsx

--- src/components/suppliers/charts/SupplierHeatmapChart.tsx
+++ src/components/suppliers/charts/SupplierHeatmapChart.tsx
@@ -121,10 +121,10 @@ export default function SupplierHeatmapChart({ suppliers, purchaseOrders, suppli
       <div className="bg-white dark:bg-gray-800 rounded-xl shadow-sm border border-gray-200 dark:border-gray-700 p-6">
         <div className="flex items-center justify-between mb-6">
 
-    
-           
-            
-            </div>
+         
+
+            {/* Toggle view mode */}
+      
           </div>
         </div>
 
